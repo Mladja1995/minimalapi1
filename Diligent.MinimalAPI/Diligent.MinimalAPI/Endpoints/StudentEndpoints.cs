0737fe5dc1e09ced4d4a02d94ebc43eac62d8602
Ilija <ilijaelfak89@gmail.com>
2022-05-23 21:55:38 +0200

    services, endpoints added

---
+++ Diligent.MinimalAPI/Diligent.MinimalAPI/Endpoints/StudentEndpoints.cs
@@ -0,0 +1,23 @@
+﻿using Diligent.MinimalAPI.Services;
+
+namespace Diligent.MinimalAPI.Endpoints
+{
+    public static class StudentEndpoints
+    {
+        // Extentions methods:
+        // Services DI
+        public static void AddStudentEndpoints(
+            this IServiceCollection services)
+        {
+            services.AddSingleton<IStudentService, StudentService>();
+        }
+
+        // Endpoints
+
+        public static void UseStudentEndpoints(
+            this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/", () => "Hello World!");
+        }
+    }
+}
