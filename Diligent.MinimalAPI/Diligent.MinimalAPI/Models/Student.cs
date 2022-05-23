d32975833670af3352289c720b78932622fc3ff1
Mladen Mladenovic <mladen.mladenovic@bullioninternational.com>
2022-05-23 19:44:26 +0200

    /

---
+++ Diligent.MinimalAPI/Diligent.MinimalAPI/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Diligent.MinimalAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int IndexNum { get; set; }
+        
+    }
+}
