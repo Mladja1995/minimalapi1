4575307d67bfd44978c2fb01a546fa1ac3f3d698
Stefan Djordjevic <stefan.djordjevic@dilig.net>
2022-05-23 21:57:46 +0200

    Book, Profesor, Project entities

---
+++ Diligent.MinimalAPI/Diligent.MinimalAPI/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿namespace Diligent.MinimalAPI.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string ProjectName { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public int ProfesorId { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+
+    }
+}
