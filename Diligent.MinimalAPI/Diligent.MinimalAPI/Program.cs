a858b22af7a3aa753bbb1e9cba438a6472872b01
Ilija <ilijaelfak89@gmail.com>
2022-05-23 23:39:51 +0200

    swagger default route added, ports changed

--- Diligent.MinimalAPI/Diligent.MinimalAPI/Program.cs
+++ Diligent.MinimalAPI/Diligent.MinimalAPI/Program.cs
@@ -18,7 +18,11 @@ builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 
 app.UseStudentEndpoints();
